ad487ee06a2b6e876fec73da0174569f3ba5343e
Dandelion-F <2604746157@qq.com>
2022-06-15 17:35:57 +0800

    fix: 修改 bg 定位

--- src/components/Background/index.vue
+++ src/components/Background/index.vue
@@ -13,10 +13,13 @@ import '../index.css'
 
 <style>
 .g-bg {
-    position: relative;
+    position: fixed;
     width: 100vw;
     height: 100vh;
-    overflow: hidden;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
 }
 
 .g-bg>div {
