b178fe6395745d6f39977e10bec153cdc074ab7c
Dandelion-F <2604746157@qq.com>
2022-06-21 20:22:21 +0800

    feat: flex -> grid

--- src/components/Counter/Operation/operation.vue
+++ src/components/Counter/Operation/operation.vue
@@ -9,10 +9,36 @@ import { operatorMap, numberMap } from './operation.js';
 
 <template>
   <div class="counter-operation">
-    <Mode></Mode>
-    <Operator :class="map"></Operator>
+    <!-- Rad/Deg -->
+    <div class="counter-mode"><Mode></Mode></div>
+    <div class="counter-delete"><Delete></Delete></div>
+    <div class="counter-number">
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Number></Number>
+      <Equal></Equal>
+    </div>
+    <!-- +-×÷ -->
+    <div class="counter-simpleBinaryOper">
+      <Operator></Operator>
+      <Operator></Operator>
+      <Operator></Operator>
+      <Operator></Operator>
+    </div>
+    <!-- () -->
+    <div class="counter-bracket">
+      <Operator></Operator>
+      <Operator></Operator>
+    </div>
     <Operator></Operator>
-    <Delete></Delete>
     <Operator></Operator>
     <Operator></Operator>
     <Operator></Operator>
@@ -24,30 +50,38 @@ import { operatorMap, numberMap } from './operation.js';
     <Operator></Operator>
     <Operator></Operator>
     <Operator></Operator>
-    <Number></Number>
-    <Number></Number>
-    <Number></Number>
     <Operator></Operator>
-    <Operator></Operator>
-    <Number></Number>
-    <Number></Number>
-    <Number></Number>
-    <Operator></Operator>
-    <Operator></Operator>
-    <Number></Number>
-    <Number></Number>
-    <Number></Number>
-    <Operator></Operator>
-    <Operator></Operator>
-    <Number></Number>
-    <Number></Number>
-    <Equal></Equal>
     <Operator></Operator>
   </div>
 </template>
 
 <style lang="postcss" scoped>
 .counter-operation {
-  @apply container z-[3] mt-4 flex flex-wrap gap-x-[5%] gap-y-4;
+  @apply container mt-4 grid grid-flow-row-dense grid-cols-5 grid-rows-[7] gap-x-[5%] gap-y-4
+  lg:grid-cols-7 lg:grid-rows-[5];
+}
+
+.counter-mode {
+  @apply col-start-1 col-end-3 flex aspect-[37/12] items-center justify-between rounded-xl bg-gray-200 shadow-inner;
+}
+
+.counter-delete {
+  @apply col-start-5 col-end-6
+  lg:col-start-7 lg:col-end-8;
+}
+
+.counter-number {
+  @apply col-start-2 col-end-5 row-start-4 row-end-[8] grid grid-cols-3 grid-rows-4 gap-y-4 gap-x-[8.62%]
+  lg:col-start-4 lg:col-end-7 lg:row-start-2 lg:row-end-6;
+}
+
+.counter-simpleBinaryOper {
+  @apply col-start-5 col-end-6 row-start-4 row-end-[8] grid gap-y-4
+  lg:col-start-7 lg:col-end-8 lg:row-start-2 lg:row-end-6;
+}
+
+.counter-bracket {
+  @apply col-start-2 col-end-4 row-start-3 row-end-4 grid grid-cols-2 gap-x-[13.5%]
+  lg:col-start-5 lg:col-end-7 lg:row-start-1 lg:row-end-2;
 }
 </style>
