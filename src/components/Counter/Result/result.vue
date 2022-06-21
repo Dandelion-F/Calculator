c5cbb6b0e54c858cafd762cc74a43b05edd59479
Dandelion-F <2604746157@qq.com>
2022-06-21 20:45:45 +0800

    fix: change result-width

--- src/components/Counter/Result/result.vue
+++ src/components/Counter/Result/result.vue
@@ -12,6 +12,7 @@ import Answer from './Answer/answer.vue';
 
 <style lang="postcss" scoped>
 .counter-result {
-  @apply container z-[3] aspect-[8/3] rounded-2xl border-t-4 border-l-4 bg-white p-[3%];
+  @apply container z-[3] aspect-[8/3] rounded-2xl border-t-4 border-l-4 bg-white p-[3%]
+  lg:aspect-[14/3];
 }
 </style>
