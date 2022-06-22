79978bd368541bc9180b3bc899f75f168b8f01e0
Dandelion-F <2604746157@qq.com>
2022-06-22 23:06:08 +0800

    chore: js -> ts

---
+++ src/components/Counter/Operation/operation.ts
@@ -0,0 +1,24 @@
+let numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0'];
+let simpleBinaryOperList = ['+', '-', '×', '÷'];
+let bracketList = ['(', ')'];
+let othersList = ['Ans', 'x^y', 'sin', 'cos', 'tan', 'log', 'x!', '%', 'ln', 'e', 'π', '√', '³√'];
+
+export { numberList, simpleBinaryOperList, bracketList, othersList };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
