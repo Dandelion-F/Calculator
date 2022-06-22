01a1d5369b23c687774c1558c762b8519fbec0ca
Dandelion-F <2604746157@qq.com>
2022-06-22 23:12:27 +0800

    feat: 引入store，添加输入运算字符

--- src/components/Counter/Operation/Operator/operator.vue
+++ src/components/Counter/Operation/Operator/operator.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts">
+<script setup>
+import useCounterStore from '../../../../store/counter';
+
 const props = defineProps({
   operator: String,
 });
+
+const counter = useCounterStore();
+const addOperator = (oper) => {
+  if (oper === '×') {
+    oper = '*';
+  }
+  if (oper === '÷') {
+    oper = '/';
+  }
+  counter.addOper(oper);
+};
 </script>
 
 <template>
-  <div class="operation-operator-btn">{{ props.operator }}</div>
+  <div
+    class="operation-operator-btn"
+    @click="() => addOperator(props.operator)"
+  >
+    {{ props.operator }}
+  </div>
 </template>
 
 <style scoped></style>
