2bf7da5c408991c43a727ed32146fd06a16aae94
Dandelion-F <2604746157@qq.com>
2022-06-28 22:33:17 +0800

    feat: 实现重置

--- src/components/Counter/Operation/Reset/reset.vue
+++ src/components/Counter/Operation/Reset/reset.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useCounterMachine from '../../../../utils/useCounterMachine.js';
+
+function handleReset() {
+  useCounterMachine
+    .onTransition((state) => {
+      console.log(state.context.result);
+    })
+    .start()
+    .send('RESET');
+}
+</script>
 
 <template>
-  <div class="operation-reset-btn">RST</div>
+  <div class="operation-reset-btn" @click="handleReset">RST</div>
 </template>
 
 <style scoped></style>
