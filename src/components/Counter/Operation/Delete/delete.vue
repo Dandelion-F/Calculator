fd036cb02e3f492bd181fcb83a311903be189292
Dandelion-F <2604746157@qq.com>
2022-07-02 20:05:12 +0800

    fix: 使用状态机删除

--- src/components/Counter/Operation/Delete/delete.vue
+++ src/components/Counter/Operation/Delete/delete.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCounterMachine } from '../../../../utils/useCounterMachine';
+
+function handleDelete() {
+  useCounterMachine.send({ type: 'DEL' });
+}
+</script>
 
 <template>
-  <div class="operation-delete-btn">DEL</div>
+  <div class="operation-delete-btn" @click="handleDelete">DEL</div>
 </template>
 
 <style scoped></style>
