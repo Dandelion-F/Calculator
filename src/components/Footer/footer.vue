38bfc1c32b1c7cf2a43236c369e5561ca9af9553
Dandelion-F <2604746157@qq.com>
2022-07-04 21:36:23 +0800

    feat: add footer (CopyRight)

--- src/components/Footer/footer.vue
+++ src/components/Footer/footer.vue
@@ -1,10 +1,11 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="footer">
-    </div>
+  <div class="footer">CopyRight@Dandelion</div>
 </template>
 
-<style>
+<style lang="postcss" scoped>
+.footer {
+  @apply fixed left-0 bottom-0 z-10 flex h-[50px] w-full items-center justify-center border-4 border-x-0 border-b-0 border-blue-200 bg-blue-100 backdrop-blur-md dark:border-gray-600 dark:bg-gray-500;
+}
 </style>
