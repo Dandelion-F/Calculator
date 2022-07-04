736459fb37ab23b512d21613a0d64a52ae9fa2cd
Dandelion-F <2604746157@qq.com>
2022-07-04 21:42:24 +0800

    fix: change footer z-index

--- src/components/Footer/footer.vue
+++ src/components/Footer/footer.vue
@@ -6,6 +6,6 @@
 
 <style lang="postcss" scoped>
 .footer {
-  @apply fixed left-0 bottom-0 z-10 flex h-[50px] w-full items-center justify-center border-4 border-x-0 border-b-0 border-blue-200 bg-blue-100 backdrop-blur-md dark:border-gray-600 dark:bg-gray-500;
+  @apply fixed left-0 bottom-0 flex h-[50px] w-full items-center justify-center border-4 border-x-0 border-b-0 border-blue-200 bg-blue-100 backdrop-blur-md dark:border-gray-600 dark:bg-gray-500;
 }
 </style>
