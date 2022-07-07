a69266a023928f9b8b9805223defb7f66d9951e0
Dandelion-F <2604746157@qq.com>
2022-07-07 22:20:02 +0800

    fix: 禁用按钮 & 添加 aria

--- src/components/Counter/Operation/Operator/operator.vue
+++ src/components/Counter/Operation/Operator/operator.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCounterMachine } from '../../../../utils/useCounterMachine';
+import { operatorMap } from '../operation.ts';
 
 const props = defineProps({
   operator: String,
@@ -8,10 +9,26 @@ const props = defineProps({
 function handleOper(operator) {
   useCounterMachine.send({ type: 'OPER', value: operator });
 }
+
+function judgeDisabledBtn(operator) {
+  let disabledBtn = true;
+
+  for (let oper in operatorMap.keys()) {
+    if (operator === oper) disabledBtn = false;
+  }
+
+  return disabledBtn;
+}
 </script>
 
 <template>
-  <div class="operation-operator-btn" @click="() => handleOper(props.operator)">
+  <div
+    class="operation-operator-btn"
+    @click="() => handleOper(props.operator)"
+    role="button"
+    :aria-label="`${operatorMap.get(props.operator)}`"
+    :aria-disabled="`${judgeDisabledBtn(props.operator)}`"
+  >
     {{ props.operator }}
   </div>
 </template>
