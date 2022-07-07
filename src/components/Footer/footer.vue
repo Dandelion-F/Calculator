226845bf04b9b0a263f6f39b4d8eb8332dd1fade
Dandelion-F <2604746157@qq.com>
2022-07-07 22:22:24 +0800

    fix: div -> footer

--- src/components/Footer/footer.vue
+++ src/components/Footer/footer.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="footer">CopyRight@Dandelion</div>
+  <footer class="bottom">CopyRight@Dandelion</footer>
 </template>
 
 <style lang="postcss" scoped>
-.footer {
+.bottom {
   @apply fixed left-0 bottom-0 flex h-[50px] w-full items-center justify-center border-4 border-x-0 border-b-0 border-blue-200 bg-blue-100 backdrop-blur-md dark:border-gray-600 dark:bg-gray-500;
 }
 </style>
