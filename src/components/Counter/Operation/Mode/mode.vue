ee0f78b7bc8331e03aff4ae633cbf0cf0c3579c6
Dandelion-F <2604746157@qq.com>
2022-07-08 21:40:40 +0800

    fix: 添加 keydown / blur 事件 & aria-pressed（mode）

--- src/components/Counter/Operation/Mode/mode.vue
+++ src/components/Counter/Operation/Mode/mode.vue
@@ -3,22 +3,68 @@ import { ref } from 'vue';
 
 let radClass = ref('operation-mode-btn');
 let degClass = ref('operation-mode-no-btn');
+let ariaRadPressed = ref(true);
+let ariaDegPressed = ref(false);
 
 function changeMode(mode: string) {
   if (mode === 'rad') {
     radClass.value = 'operation-mode-btn';
     degClass.value = 'operation-mode-no-btn';
+    ariaRadPressed.value = true;
+    ariaDegPressed.value = false;
   } else {
     degClass.value = 'operation-mode-btn';
     radClass.value = 'operation-mode-no-btn';
+    ariaRadPressed.value = false;
+    ariaDegPressed.value = true;
+  }
+}
+
+function handleKeydown(e: any, mode: string) {
+  if (e.code === 'Enter') {
+    changeMode(mode);
+  }
+}
+
+function handleBlur(mode: string) {
+  if (mode === 'rad') {
+    ariaRadPressed.value = false;
+  } else {
+    ariaDegPressed.value = false;
   }
 }
 </script>
 
 <template>
   <div class="operation-mode-box">
-    <div :class="radClass" @click="() => changeMode('rad')" role="button" aria-label="弧度模式" aria-disabled="true">Rad</div>
-    <div :class="degClass" @click="() => changeMode('deg')" role="button" aria-label="角度模式" aria-disabled="true">Deg</div>
+    <div
+      :class="radClass"
+      id="Rad"
+      @click="() => changeMode('rad')"
+      role="button"
+      tabindex="0"
+      aria-label="弧度模式"
+      aria-disabled="true"
+      @keydown="(e) => handleKeydown(e, 'rad')"
+      :aria-pressed="`${ariaRadPressed}`"
+      @blur="() => handleBlur('rad')"
+    >
+      Rad
+    </div>
+    <div
+      :class="degClass"
+      id="Deg"
+      @click="() => changeMode('deg')"
+      role="button"
+      tabindex="0"
+      aria-label="角度模式"
+      aria-disabled="true"
+      @keydown="(e) => handleKeydown(e, 'deg')"
+      :aria-pressed="`${ariaDegPressed}`"
+      @blur="() => handleBlur('deg')"
+    >
+      Deg
+    </div>
   </div>
 </template>
 
@@ -28,7 +74,7 @@ function changeMode(mode: string) {
 }
 
 .operation-mode-no-btn {
-  @apply flex aspect-[4/3] h-full items-center justify-center text-xl font-bold text-gray-400 cursor-pointer
+  @apply flex aspect-[4/3] h-full cursor-pointer items-center justify-center text-xl font-bold text-gray-400
   lg:text-2xl;
 }
 </style>
