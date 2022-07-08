d5fb7b88d6e9dd132b2ac06ef8e9ddb6ae7eed7b
Dandelion-F <2604746157@qq.com>
2022-07-08 21:41:04 +0800

    fix: 添加 keydown / blur 事件 & aria-pressed（number）

--- src/components/Counter/Operation/Number/number.vue
+++ src/components/Counter/Operation/Number/number.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { ref } from 'vue';
 import { useCounterMachine } from '../../../../utils/useCounterMachine';
 import { numberMap } from '../operation.ts';
+
+let ariaPressed = ref(false);
 
 const props = defineProps({
   number: String,
@@ -16,15 +19,31 @@ function handleInt(number) {
     type = 'INT';
   }
   useCounterMachine.send({ type, value: number });
+  ariaPressed.value = true;
+}
+
+function handlekeydown(e, number) {
+  if (e.code === 'Enter') {
+    handleInt(number);
+  }
+}
+
+function handleBlur() {
+  ariaPressed.value = false;
 }
 </script>
 
 <template>
   <div
     class="operation-number-btn"
+    :id="`${props.number}`"
     @click="() => handleInt(props.number)"
     role="button"
+    tabindex="0"
+    :aria-pressed="`${ariaPressed}`"
     :aria-label="`${numberMap.get(props.number)}`"
+    @keydown="(e) => handlekeydown(e, props.number)"
+    @blur="handleBlur"
   >
     {{ props.number }}
   </div>
