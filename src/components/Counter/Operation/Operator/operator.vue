af5bfef01f6f09508d7d8d988be9450ade881dfe
Dandelion-F <2604746157@qq.com>
2022-07-08 21:41:25 +0800

    fix: 添加 keydown / blur 事件 & aria-pressed（operator）

--- src/components/Counter/Operation/Operator/operator.vue
+++ src/components/Counter/Operation/Operator/operator.vue
@@ -1,33 +1,59 @@
 <script setup>
+import { ref } from 'vue';
 import { useCounterMachine } from '../../../../utils/useCounterMachine';
-import { operatorMap } from '../operation.ts';
+import { operatorMap, simpleBinaryOperList } from '../operation.ts';
+
+let ariaPressed = ref(false);
 
 const props = defineProps({
   operator: String,
 });
 
 function handleOper(operator) {
-  useCounterMachine.send({ type: 'OPER', value: operator });
+  if (!judgeDisabledBtn(operator)) {
+    ariaPressed.value = true;
+    useCounterMachine.send({ type: 'OPER', value: operator });
+  }
 }
 
 function judgeDisabledBtn(operator) {
   let disabledBtn = true;
 
-  for (let oper in operatorMap.keys()) {
-    if (operator === oper) disabledBtn = false;
+  for (let oper of simpleBinaryOperList) {
+    if (operator === oper) {
+      disabledBtn = false;
+      break;
+    }
   }
 
   return disabledBtn;
+}
+
+function handlekeydown(e, operator) {
+  if (e.code === 'Enter') {
+    handleOper(operator);
+  }
+}
+
+function handleBlur() {
+  if (!judgeDisabledBtn(operator)) {
+    ariaPressed.value = false;
+  }
 }
 </script>
 
 <template>
   <div
     class="operation-operator-btn"
+    :id="`${props.operator}`"
     @click="() => handleOper(props.operator)"
     role="button"
+    tabindex="0"
+    :aria-pressed="`${ariaPressed}`"
     :aria-label="`${operatorMap.get(props.operator)}`"
     :aria-disabled="`${judgeDisabledBtn(props.operator)}`"
+    @keydown="(e) => handlekeydown(e, props.operator)"
+    @blur="() => handleBlur(props.operator)"
   >
     {{ props.operator }}
   </div>
