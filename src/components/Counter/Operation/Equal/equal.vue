a8c5ec8559eb36a381f12607625af9e32332ef08
Dandelion-F <2604746157@qq.com>
2022-07-08 21:39:01 +0800

    fix: 添加 keydown / blur 事件 & aria-pressed（equal）

--- src/components/Counter/Operation/Equal/equal.vue
+++ src/components/Counter/Operation/Equal/equal.vue
@@ -1,13 +1,39 @@
 <script setup>
+import { ref } from 'vue';
 import { useCounterMachine } from '../../../../utils/useCounterMachine';
 
+let ariaPressed = ref(false);
+
 function handleEqual() {
+  ariaPressed.value = true;
   useCounterMachine.send({ type: 'EQUAL', value: '=' });
+}
+
+function handlekeydown(e) {
+  if (e.code === 'Enter') {
+    handleEqual();
+  }
+}
+
+function handleBlur() {
+  ariaPressed.value = false;
 }
 </script>
 
 <template>
-  <div class="operation-equal-btn" @click="handleEqual" role="button" aria-label="等于">=</div>
+  <div
+    class="operation-equal-btn"
+    id="="
+    @click="handleEqual"
+    role="button"
+    aria-label="等于"
+    tabindex="0"
+    @keydown="(e) => handlekeydown(e)"
+    :aria-pressed="`${ariaPressed}`"
+    @blur="handleBlur"
+  >
+    =
+  </div>
 </template>
 
 <style scoped></style>
