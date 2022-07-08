2f18bff3d905f857e3ef2608e3abaf306ecbe548
Dandelion-F <2604746157@qq.com>
2022-07-08 21:38:19 +0800

    fix: 添加 keydown 和 blur 事件（delete）

--- src/components/Counter/Operation/Delete/delete.vue
+++ src/components/Counter/Operation/Delete/delete.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useCounterMachine } from '../../../../utils/useCounterMachine';
 
+let ariaPressed = ref(false);
+
 function handleDelete() {
+  ariaPressed.value = true;
   useCounterMachine.send({ type: 'DEL' });
+}
+
+function handlekeydown(e: any) {
+  if (e.code === 'Enter') {
+    handleDelete();
+  }
+}
+
+function handleBlur(e: any) {
+  ariaPressed.value = false;
 }
 </script>
 
 <template>
-  <div class="operation-delete-btn" @click="handleDelete" role="button" aria-label="删除">DEL</div>
+  <div
+    class="operation-delete-btn"
+    id="DEL"
+    @click="handleDelete"
+    role="button"
+    aria-label="删除"
+    :aria-pressed="`${ariaPressed}`"
+    @keydown="(e) => handlekeydown(e)"
+    tabindex="0"
+    @blur="handleBlur"
+  >
+    DEL
+  </div>
 </template>
 
 <style scoped></style>
