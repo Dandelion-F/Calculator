410f7c158c1b7224ffedd857760f7642d4658525
Dandelion-F <2604746157@qq.com>
2022-07-08 21:41:36 +0800

    fix: 添加 keydown / blur 事件 & aria-pressed（reset）

--- src/components/Counter/Operation/Reset/reset.vue
+++ src/components/Counter/Operation/Reset/reset.vue
@@ -1,13 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useCounterMachine } from '../../../../utils/useCounterMachine';
 
+let ariaPressed = ref(false);
+
 function handleReset() {
+  ariaPressed.value = true;
   useCounterMachine.send({ type: 'RESET' });
+}
+
+function handlekeydown(e: any) {
+  if (e.code === 'Enter') {
+    handleReset();
+  }
+}
+
+function handleBlur() {
+  ariaPressed.value = false;
 }
 </script>
 
 <template>
-  <div class="operation-reset-btn" @click="handleReset" role="button" aria-label="重置">RST</div>
+  <div
+    class="operation-reset-btn"
+    id="RST"
+    @click="handleReset"
+    role="button"
+    aria-label="重置"
+    tabindex="0"
+    :aria-pressed="`${ariaPressed}`"
+    @keydown="(e) => handlekeydown(e)"
+    @blur="handleBlur"
+  >
+    RST
+  </div>
 </template>
 
 <style scoped></style>
