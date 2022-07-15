6ca819422e5646f1a0105838db11fb7c4f5b0a14
Dandelion-F <2604746157@qq.com>
2022-07-15 16:28:46 +0800

    feat: 添加 VitePWA 配置，包含 manifest 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { VitePWA, ManifestOptions } from 'vite-plugin-pwa';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+let manifest: Partial<ManifestOptions> = {
+  name: '计算器',
+  description: 'A simple demo.',
+  background_color: '#F1FAFA',
+  theme_color: '#00B271',
+};
+
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico'],
+      manifest: manifest,
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /(.*?)\.(js|css|ts)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'js-css-cache',
+            },
+          },
+          {
+            urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
