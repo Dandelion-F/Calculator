a002e15007036e66f95f5fc3d63da68877e36bc7
Dandelion-F <2604746157@qq.com>
2022-07-15 16:30:13 +0800

    feat: 引入 useRegisterSW，注册 service worker

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import { useRegisterSW } from 'virtual:pwa-register/vue';
 import './App.css';
+
+useRegisterSW();
 
 const pinia = createPinia();
 const app = createApp(App);
